04403af1cac83227502d0dec7840f8faa0e00b8f
Platykin <igor.alves.pinto11@gmail.com>
2024-06-10 14:43:44 -0300

    First cmake build directory made

---
+++ build/CMakeFiles/Vibration_Analysis.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vibration_Analysis.dir/main.cpp.obj"
+  "CMakeFiles/Vibration_Analysis.dir/main.cpp.obj.d"
+  "Vibration_Analysis.exe"
+  "Vibration_Analysis.exe.manifest"
+  "Vibration_Analysis.pdb"
+  "libVibration_Analysis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vibration_Analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
